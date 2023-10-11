77848b5ddfa8efcaedf14804998d8e901438ae4a
Hollson <Hollson@qq.com>
2023-10-11 17:35:20 +0800

    Dockerfile配置时区信息

--- docker/hello_go/main.go
+++ docker/hello_go/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"net/http"
+	"time"
+	"fmt"
+	"os"
 )
 
+// Go项目构建Docker镜像，并验证时区等系统信息
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("<h1>hello world</h1>"))
+		fmt.Fprintf(w, "<h1>Hello world</h1> <h2>时间：%s</h2> <h2>时区：%s</h2>" ,time.Now().Format("2006-01-02 15:04:05"), os.Getenv("TZ"))
 	})
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
