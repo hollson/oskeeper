452ef3159d8241a65b129df359737a876ffca617
hollson <hollson@qq.com>
2023-10-11 22:19:38 +0800

    optmize dockerfile

--- docker/hello_go/main.go
+++ docker/hello_go/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"net/http"
-	"time"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 )
 
 // Go项目构建Docker镜像，并验证时区等系统信息
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprintf(w, "<h1>Hello world</h1> <h2>时间：%s</h2> <h2>时区：%s</h2>" ,time.Now().Format("2006-01-02 15:04:05"), os.Getenv("TZ"))
+		fmt.Fprintf(w, "<h1>Hello world</h1>  <h2>时间：%s</h2> <h2>时区：%s</h2>",
+			time.Now().Format("2006-01-02 15:04:05"), os.Getenv("TZ"))
 	})
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
--- docker/main.go
+++
@@ -1,6 +0,0 @@
-package main
-
-//go:generate go build -ldflags="-s -w" -o hello-server main.go
-func main() {
-	println("hello world!")
-}
