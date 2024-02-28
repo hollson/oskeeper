9a4b2b396e34ac01dea29bd72d2fe0cecd190f21
hollson <hollson@qq.com>
2024-02-29 02:34:08 +0800

    Rebuilded at 2024/02/29 02:34:08

--- term/main.go
+++ term/main.go
@@ -1,138 +1,172 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"os/exec"
 	"strconv"
+	"strings"
 
 	"github.com/jedib0t/go-pretty/v6/table"
 )
 
-const (
-	ColorHeader  = "\033[95m"
-	ColorBlue    = "\033[94m"
-	ColorRed     = "\033[91m"
-	ColorGreen   = "\033[92m"
-	ColorWarning = "\033[93m"
-	ColorFail    = "\033[91m"
-	ColorEnd     = "\033[0m"
-	ColorBold    = "\033[1m"
-)
-
-// Host 主机信息
+// Host represents a host configuration.
 type Host struct {
-	ID   int
-	Host string
-	Port int
-	User string
-	Key  string
-	OS   string
-	Arch string
-	Addr string
-	Mark string
+	ID     int    `json:"id"`
+	Host   string `json:"host"`
+	Port   int    `json:"port"`
+	User   string `json:"user"`
+	Key    string `json:"key"`
+	OS     string `json:"os"`
+	Arch   string `json:"arch"`
+	Region string `json:"region"`
+	Mark   string `json:"mark"`
 }
 
-func loadHosts(filePath string) []Host {
-	// 读取主机信息
+// Color represents ANSI color codes.
+type Color string
+
+const (
+	Bold  Color = "\033[1m"
+	Blue  Color = "\033[94m"
+	Green Color = "\033[92m"
+	Red   Color = "\033[91m"
+	End   Color = "\033[0m"
+)
+
+func (t Color) string() string {
+	return string(t)
+}
+
+func loadHosts(configPath string) ([]Host, error) {
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
 	var hosts []Host
-	// 这里你可以根据实际需要使用 Go 的文件操作函数读取 JSON 文件并解析成结构体切片
-	// 此处省略，你可以在这里添加你的文件读取和 JSON 解析逻辑
+	if err := json.Unmarshal(data, &hosts); err != nil {
+		return nil, err
+	}
 
-	// 临时示例数据
-	hosts = append(hosts, Host{1, "192.168.0.1", 22, "root", "", "ubuntu22.04", "arm64", "新加坡", "我的开发虚拟机"})
-	hosts = append(hosts, Host{2, "192.168.0.2", 22, "root", "", "ubuntu22.04", "arm64", "新加坡002", "我的开发虚拟机"})
+	// Check for duplicate IDs
+	idSet := make(map[int]bool)
+	for _, host := range hosts {
+		if idSet[host.ID] {
+			return nil, fmt.Errorf("duplicate host ID: %d", host.ID)
+		}
+		idSet[host.ID] = true
+	}
 
-	return hosts
+	return hosts, nil
+}
+
+func createSample(filePath string) {
+	data := []Host{
+		{ID: 1001, Host: "192.168.10.1", User: "root", OS: "CentOS Stream 9", Arch: "x86_64", Region: "香港", Mark: "开发环境"},
+		{ID: 1002, Host: "example.com", User: "root", OS: "ubuntu22.04", Arch: "arm64", Region: "新加坡", Mark: "生产环境"},
+		{ID: 1003, Host: "example.com", User: "root", Mark: "测试环境"},
+	}
+
+	fileData, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		fmt.Printf("Error creating sample file: %v\n", err)
+		return
+	}
+
+	err = ioutil.WriteFile(filePath, fileData, 0644)
+	if err != nil {
+		fmt.Printf("Error creating sample file: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Sample file created: %s\n", filePath)
 }
 
 func printHosts(hosts []Host) {
-	// 创建表格
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"ID", "主机地址", "用户", "操作系统", "架构", "位置", "备注"})
+	t.AppendHeader(table.Row{
+		Bold + Blue + "ID" + End,
+		Bold + Blue + "主机" + End,
+		Bold + Blue + "用户" + End,
+		Bold + Blue + "系统" + End,
+		Bold + Blue + "架构" + End,
+		Bold + Blue + "位置" + End,
+		Bold + Blue + "备注" + End,
+	})
 
-	// 添加数据行
 	for _, host := range hosts {
-		t.AppendRow(table.Row{host.ID, host.Host, host.User, host.OS, host.Arch, host.Addr, host.Mark})
+		id := strconv.Itoa(host.ID)
+		hostName := host.Host
+		userName := host.User
+		osInfo := host.OS
+		archInfo := host.Arch
+		regionInfo := host.Region
+		markInfo := host.Mark
+
+		if id != "" && hostName != "" && userName != "" {
+			if id != "" || hostName != "" || userName != "" || osInfo != "" || archInfo != "" || regionInfo != "" || markInfo != "" {
+				t.AppendRow([]interface{}{Green.string() + id + End.string(), Green.string() + hostName + End.string(), Green.string() + userName + End.string(), osInfo, archInfo, regionInfo, markInfo})
+			}
+		}
 	}
 
-	// 设置样式
-	t.SetStyle(table.StyleLight)
-
-	// 输出表格
-	fmt.Println(fmt.Sprintf("%s\t\t\t(Linux)SSH远程主机管理 v1.0 \t\t\t\t %s", ColorHeader, ColorEnd))
-	t.Render()
+	if t.Length() > 0 {
+		fmt.Printf("%s(Linux)SSH远程主机管理\t\t\t\t%s\n", Bold+Blue, End)
+		t.Render()
+	} else {
+		fmt.Printf("%s所有行都为空，无法生成表格%s\n", Red, End)
+	}
 }
 
 func sshConnect(host Host) {
-	// 连接到远程主机
-	trySSHConnect(host)
-}
-
-func trySSHConnect(host Host) {
-	// 尝试 SSH 连接
 	portOption := ""
 	if host.Port != 22 {
 		portOption = fmt.Sprintf("-p %d", host.Port)
 	}
 
 	keyOption := ""
-	if host.Key != "" && host.Key != "null" {
+	if host.Key != "" && strings.ToLower(host.Key) != "null" {
 		keyOption = fmt.Sprintf("-i %s", host.Key)
 	}
 
-	cmd := exec.Command("ssh", portOption, keyOption, fmt.Sprintf("%s@%s", host.User, host.Host))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
+	cmd := fmt.Sprintf("ssh %s %s %s@%s", portOption, keyOption, host.User, host.Host)
+	fmt.Printf("%s=> 正在尝试登录: %s%s\n", Green, Red.string()+cmd+End.string(), End)
 
-	fmt.Printf("%s=> 正在尝试登录: %s %s\n", ColorGreen, ColorEnd, fmt.Sprintf("%s@%s", host.User, host.Host))
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("%s连接到 %s 时发生错误：%s%s\n", ColorFail, host.Host, err, ColorEnd)
-	}
+	// Add your SSH connection logic here
 }
 
 func main() {
-	// 读取主机信息
-	filePath := "./hosts.json"
-	hosts := loadHosts(filePath)
+	configPath := "./term_hosts_example.json"
+	hosts, err := loadHosts(configPath)
+	if err != nil {
+		fmt.Printf("Error loading hosts: %v\n", err)
+		return
+	}
 
-	// 打印主机信息
 	printHosts(hosts)
 
-	// 选择主机
-	selectedID := getUserInput(fmt.Sprintf("%s请输入主机ID：%s", ColorBold, ColorEnd))
-	selectedHost := getHostByID(hosts, selectedID)
-
-	if selectedHost != nil {
-		// 尝试连接到远程主机
-		sshConnect(*selectedHost)
-	} else {
-		fmt.Printf("%s无效的主机ID。退出。%s\n", ColorFail, ColorEnd)
-	}
-}
-
-func getUserInput(prompt string) int {
-	// 获取用户输入并转换为整数
-	var userInput string
-	fmt.Print(prompt)
-	fmt.Scanln(&userInput)
-	id, err := strconv.Atoi(userInput)
+	var id int
+	fmt.Printf("%s%s请输入主机ID：%s", Bold+Blue, End, End)
+	_, err = fmt.Scan(&id)
 	if err != nil {
-		fmt.Printf("%s无效的输入。请输入有效的ID。%s\n", ColorFail, ColorEnd)
-		return getUserInput(prompt)
+		fmt.Printf("%s输入无效，退出。%s\n", Red, End)
+		return
 	}
-	return id
-}
 
-func getHostByID(hosts []Host, id int) *Host {
-	// 根据主机ID获取主机信息
+	var selectedHost Host
 	for _, host := range hosts {
 		if host.ID == id {
-			return &host
+			selectedHost = host
+			break
 		}
 	}
-	return nil
+
+	if selectedHost.ID != 0 {
+		sshConnect(selectedHost)
+	} else {
+		fmt.Printf("%s参数错误，退出。%s\n", Red, End)
+	}
 }
